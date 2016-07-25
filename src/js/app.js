11f0139d450ee79c1d27d9f3d4325821a8b22cdf
Lightman <yaruk.my@gmail.com>
2016-07-25 13:39:37 +0300

    feat: add #getPartials

--- src/js/app.js
+++ src/js/app.js
@@ -6,10 +6,19 @@ module.exports = {
 		return templateEl.innerHTML;
 	},
 
-	render: function(templateName, templateVars) {
+	render: function(templateName, templateVars, partials) {
 		var template = this.getHtml(templateName);
+		var partialsObj = this.getPartials(partials);
 		if (!template) return false;
 		return Mustache.render(template, templateVars);
+	},
+
+	getPartials: function(partialsList) {
+		var obj = {};
+		partialsList.forEach(function(name) {
+			obj[name] = this.getHtml(name);
+		}, this);
+		return obj;
 	},
 
 	stringRender: function(string, data) {
